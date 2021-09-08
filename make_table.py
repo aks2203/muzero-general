7c907f2963b317a3f69b385aab30bcba5c723185
Avi Schwarzschild <avi1@math.umd.edu>
2021-09-08 09:22:13 -0400

    launch file added, and make_table.py updated.

--- make_table.py
+++ make_table.py
@@ -1,11 +1,6 @@
 import pandas as pd
 
-df = pd.read_pickle("results/connect4/pickled_dfs/2021-09-02--16-55-31+2021-09-02--16-55-40/"
-                    # "2021-09-03--17-10-56.pkl"
-                    # "2021-09-03--17-13-33.pkl"
-                    # "2021-09-03--17-17-07.pkl"
-                    # "2021-09-03--17-17-55.pkl"
-                    )
+df = pd.read_pickle("results/connect4/pickled_dfs/29951e5a0d0011ec88923cfdfed40170+29951e5a0d0011ec88923cfdfed40170/d2ae0f620e4d11ecb2053cfdfed40144.pkl")
 
 print(df.to_markdown())
 df_1_went_first = df[df['who_goes_first'] == 1]
